b1b1843057ddd1a4f78120a9fa0b719dd07af967
alaverArt <alaverArt@yandex.ru>
2022-08-16 23:08:08 +0300

    промежуточный коммит

--- main.js
+++ main.js
@@ -2,13 +2,21 @@ const item_labels = document.querySelectorAll('.accordion__label');
 item_labels.forEach( (el) => {
     el.addEventListener('click', (e) => {
         const elem = e.target;
-        const parent = elem.parentNode;
+        let parent = elem.parentNode;
         const body = parent.querySelector('.accordion__body');
 
         parent.classList.toggle('accordion__item_active');
         
         if(parent.classList.contains('accordion__item_active')){
             body.style.maxHeight = body.scrollHeight + 'px';
+
+            while(!parent.classList.contains('accordion')){
+                parent = parent.parentNode;
+                const tempBody = parent.querySelector('.accordion__body');
+                if(tempBody && parent.classList.contains('accordion__item'))
+                    tempBody.style.maxHeight = tempBody.scrollHeight + parent.style.height + 'px';
+            }
+
         }    
         else{
             body.style.maxHeight = '0';
